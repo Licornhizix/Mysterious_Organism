5c32010561c876bb8564268ce5f5b0de3d91b103
Julien <licornhizix@gmail.com>
2023-08-16 21:38:24 +0200

    fonction mutate

--- main.js
+++ main.js
@@ -1,27 +1,40 @@
 // Returns a random DNA base
 const returnRandBase = () => {
-    const dnaBases = ['A', 'T', 'C', 'G']
-    return dnaBases[Math.floor(Math.random() * 4)] 
+  const dnaBases = ['A', 'T', 'C', 'G']
+  return dnaBases[Math.floor(Math.random() * 4)] 
+}
+
+
+// Returns a random single stand of DNA containing 15 bases
+const mockUpStrand = () => {
+  const newStrand = []
+  for (let i = 0; i < 15; i++) {
+    newStrand.push(returnRandBase())
   }
-  
-  // Returns a random single stand of DNA containing 15 bases
-  const mockUpStrand = () => {
-    const newStrand = []
-    for (let i = 0; i < 15; i++) {
-      newStrand.push(returnRandBase())
+  return newStrand
+}
+const pAequorFactory= (number, array) => {
+  return {
+    specimenNum : number,
+    dna : array,
+    mutate() {
+      let newBase = returnRandBase();
+      let pickRandom = Math.floor(Math.random() * 15);
+      do{
+        newBase = returnRandBase();
+      }while(newBase === this.dna[pickRandom]);
+      this.dna[pickRandom] = newBase;
+      return this.dna
+    } 
     }
-    return newStrand
-  }
-  function pAequorFactory(number, array) {
-    return {
-      specimenNum : specimenNum,
-      dna : dna,
-      } 
-  };
-  
-  
-  
-  
-  
-  
-  
+    }
+
+
+pAequorFactory();
+console.log(pAequorFactory(1, mockUpStrand()));
+const test = pAequorFactory(1, mockUpStrand());
+console.log(test.mutate());
+
+
+
+
